883e95b725827a4ee7f57a018207ac7e4e2d41f1
Stephen Riesenberg <sjohnr@hotmail.com>
2018-09-02 17:07:46 -0500

    Implemented endpoints

--- api/src/main/kotlin/io/insource/api/demo/controller/TeamsController.kt
+++ api/src/main/kotlin/io/insource/api/demo/controller/TeamsController.kt
@@ -1,7 +1,10 @@
 package io.insource.api.demo.controller
 
+import io.insource.api.demo.service.NflOfficialMapper
 import io.insource.api.v1.teams.Team
+import io.insource.api.v1.teams.TeamSummary
 import io.insource.api.v1.teams.players.Player
+import io.insource.api.v1.teams.players.PlayerSummary
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import org.springframework.http.MediaType
@@ -9,23 +12,36 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import us.sportradar.api.official.NflOfficialApi
 
 @Api(tags = ["teams"])
 @RestController
 @RequestMapping("/api/v1", produces = [MediaType.APPLICATION_JSON_VALUE])
-class TeamsController {
+class TeamsController(
+  val nflOfficialApi: NflOfficialApi,
+  val nflOfficialMapper: NflOfficialMapper
+) {
+  @ApiOperation("Get a list of teams.")
+  @GetMapping("/teams")
+  fun teams(): List<TeamSummary> = nflOfficialApi.league().let {
+    nflOfficialMapper.mapTeamSummary(it)
+  }
+
   @ApiOperation("Get information about a team.")
   @GetMapping("/teams/{teamId}")
-  fun team(@PathVariable("teamId") id: String): Team = readResource("team.json", Team::class)
+  fun team(@PathVariable teamId: String): Team = nflOfficialApi.team(teamId).let { profileResponse ->
+    nflOfficialMapper.mapTeam(profileResponse)
+  }
 
   @ApiOperation("Get a list of a team's players.")
   @GetMapping("/teams/{teamId}/players")
-  fun players(@PathVariable("teamId") id: String): List<Player> = ArrayList<Player>().also {
-    it.add(readResource("player.json", Player::class))
-    it.add(readResource("player.json", Player::class))
+  fun players(@PathVariable teamId: String): List<PlayerSummary> = nflOfficialApi.team(teamId).let { profileResponse ->
+    nflOfficialMapper.mapPlayerSummary(profileResponse)
   }
 
   @ApiOperation("Get information about a player.")
   @GetMapping("/teams/{teamId}/players/{playerId}")
-  fun player(@PathVariable("teamId") teamId: String, @PathVariable("playerId") playerId: String): Player = readResource("player.json", Player::class)
+  fun player(@PathVariable teamId: String, @PathVariable playerId: String): Player = nflOfficialApi.player(playerId).let { playerResponse ->
+    nflOfficialMapper.mapPlayer(playerResponse)
+  }
 }
